b8c4a802f7872f439145a6a518a34b685dd472b8
Santiago91218 <141759544+Santiago91218@users.noreply.github.com>
2023-09-19 19:18:48 -0300

    Repaso Parcial

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        //Ejercicio 1
+        /*int num1, num2;
+        System.out.println("Ingrese un numero: ");
+        num1= sc.nextInt();
+        System.out.println("Ingrese otro numero: ");
+        num2= sc.nextInt();
+
+        if (num1%10==num2%10){
+            System.out.println("La ultima cifra de los numeros son iguales");
+        }else{
+            System.out.println("La ultima cifra de los numeros no son iguales");
+        }*/
+
+        //Ejercicio 2
+        /*System.out.println("Ingrese un numero entero de 3 cifras: ");
+        int num= sc.nextInt();
+
+        if (num<100 || num>999){
+            System.out.println("El numero ingresado no tiene 3 cifras.");
+        }else {
+            if(num/100==num%10){
+                System.out.println("El numero es capicua");
+            }else{
+                System.out.println("El numero no es capicua");
+            }
+        }*/
+    }
+}
