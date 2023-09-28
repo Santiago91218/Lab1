548bf60c80486aa51e2324ce9706f02c0b29076c
Santiago91218 <141759544+Santiago91218@users.noreply.github.com>
2023-09-28 09:24:09 -0300

    Parcial1-Comeglio Santiago-Comision3

--- Main.java
+++ Main.java
@@ -1,33 +1,58 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
+        //Decalaracion de variables
+        int num1,num2,opcion;
+        boolean condicion;
+        num1=num2=opcion=0;
+        condicion=true;
 
-        //Ejercicio 1
-        /*int num1, num2;
-        System.out.println("Ingrese un numero: ");
-        num1= sc.nextInt();
-        System.out.println("Ingrese otro numero: ");
-        num2= sc.nextInt();
+        while(opcion!=5){
+            //Le pedimos al usuario el eliga una opcion
+            System.out.println("Eliga una opcion (ingrese el numero de la opcion): 1)Suma 2)Resta 3)Multiplicacion 4)Division 5)Salir");
+            opcion= sc.nextInt();
 
-        if (num1%10==num2%10){
-            System.out.println("La ultima cifra de los numeros son iguales");
-        }else{
-            System.out.println("La ultima cifra de los numeros no son iguales");
-        }*/
-
-        //Ejercicio 2
-        /*System.out.println("Ingrese un numero entero de 3 cifras: ");
-        int num= sc.nextInt();
-
-        if (num<100 || num>999){
-            System.out.println("El numero ingresado no tiene 3 cifras.");
-        }else {
-            if(num/100==num%10){
-                System.out.println("El numero es capicua");
-            }else{
-                System.out.println("El numero no es capicua");
+            //Validamos la opcion elegida
+            if(opcion>=1 && opcion<=4){
+                System.out.println("Ingrese un numero: ");
+                num1= sc.nextInt();
+                System.out.println("Ingrese otro numero: ");
+                num2= sc.nextInt();
             }
-        }*/
+
+            if(opcion==1){
+                System.out.println("Has elegido Suma");
+                //Sumamos los numeros ingresados y mostramos por pantalla el resultado
+                int suma=num1+num2;
+                System.out.println("El resultado de la operacion es: "+suma);
+            }else if(opcion==2){
+                System.out.println("Has elegido Resta");
+                //Restamos los numeros ingresados y mostramos por pantalla el resultado
+                int resta=num1-num2;
+                System.out.println("El resultado de la operacion es: "+resta);
+            }else if(opcion==3){
+                System.out.println("Has elegido Multiplicacion");
+                //Multiplicamos los numeros ingresados y mostramos por pantalla el resultado
+                int multp=num1*num2;
+                System.out.println("El resultado de la operacion es: "+multp);
+            }else if(opcion==4){
+                System.out.println("Has elegido Division");
+                if(num2==0){
+                    //Validamos la division por 0
+                    System.out.println("No se puede dividir por 0");
+                }else{
+                    //Dividimos los numeros ingresados y mostramos por pantalla el resultado
+                    double div=num1/num2;
+                    System.out.println("El resultado de la operacion es: "+div);
+                }
+            }else if(opcion==5){
+                //Mostamos por pantalla un mensaje indicando el fin de la ejecucion del pograma
+                System.out.println("Saliendo...");
+            }else{
+                //Validamos que la opcion ingresada
+                System.out.println("La opcion ingresada no es valida");
+                }
+            }
+        }
     }
-}
